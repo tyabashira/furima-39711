23ff80c285f6e67aa2b5ae0a2b288bcf7deb49cc
tyabashira <ponponsokoku37@gmail.com>
2023-09-12 18:39:58 +0900

    整数値のみバリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,7 +12,7 @@ class Item < ApplicationRecord
   validates :item_name, presence: true, length: { maximum:40, message:"40文字以下にしてください" }
   validates :item_text, presence: true, length: { maximum:1000, message:"1000文字以下にしてください" }
 
-  validates :price, numericality:{ greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "は ¥300~9,999,999 の間で半角数字で指定してください" }
+  validates :price, numericality:{ greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, only_integer: true, message: "は ¥300~9,999,999 の間で半角数字で指定してください" }
 
   validates :category_id, numericality: { other_than: 1,  message: "can't be blank"} 
   validates :cost_id, numericality: { other_than: 1,  message: "can't be blank"} 
