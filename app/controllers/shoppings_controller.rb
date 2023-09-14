8520aad4906961939b80e7f16a1e9c0b4fef3ceb
tyabashira <ponponsokoku37@gmail.com>
2023-09-14 20:55:36 +0900

    修正

--- app/controllers/shoppings_controller.rb
+++ app/controllers/shoppings_controller.rb
@@ -1,22 +1,22 @@
 class ShoppingsController < ApplicationController
-  def index
-    @shopping = Shopping.new
-   end
+  # def index
+  #   @shopping = Shopping.new
+  #  end
 
-    def create
-    @shopping = Shopping.new(shopping_params)
-    binding.pry
-    if @shopping.valid?
-      @shopping.save
-      return redirect_to root_path
-    else
-      render 'index', status: :unprocessable_entity
-    end
-  end
+  #   def create
+  #   @shopping = Shopping.new(shopping_params)
+  #   binding.pry
+  #   if @shopping.valid?
+  #     @shopping.save
+  #     return redirect_to root_path
+  #   else
+  #     render 'index', status: :unprocessable_entity
+  #   end
+  # end
 
-  private
+  # private
 
-  def shopping_params
-    params.require(:shopping).permit(:post_code, :states_id, :city, :street, :build, :phone)
-  end
-
+  # def shopping_params
+  #   params.require(:shopping).permit(:post_code, :states_id, :city, :street, :build, :phone)
+  # end
+end
--- app/models/shopping.rb
+++ app/models/shopping.rb
@@ -1,10 +1,10 @@
  class Shopping < ApplicationRecord
-   belongs_to :furima
-   belongs_to :states
+  #  belongs_to :furima
+  #  belongs_to :states
 
-   validates :post_code, presence: true
-   validates :states, numericality: { other_than: 0,  message: "can't be blank"}
-   validates :city, presence: true
-   validates :street, presence: true
-  validates :phone, presence: true
+  #  validates :post_code, presence: true
+  #  validates :states, numericality: { other_than: 0,  message: "can't be blank"}
+  #  validates :city, presence: true
+  #  validates :street, presence: true
+  # validates :phone, presence: true
  end
