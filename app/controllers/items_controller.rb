d7f992026d6d53b896cb1d955418f36344d5dd42
tyabashira <ponponsokoku37@gmail.com>
2023-09-15 19:40:02 +0900

    username以外まで

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -17,6 +17,9 @@ end
   end
 end
 
+def show
+  @item = Item.find(params[:id])
+end
  private
  def item_params
    params.require(:item).permit(:image, :item_name, :item_text, :category_id, :quality_id, :cost_id, :region_id, :post_day_id, :price, :user).merge(user_id: current_user.id)
